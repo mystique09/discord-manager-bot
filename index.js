886c19f55483929698c7049831596d5ab839c0ab
mystique09 <knightkrypto7@gmail.com>
2021-05-24 01:07:39 +0800

    modified index.js to receive new members

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
-
-const {CommandoClient} = require('discord.js-commando');
+const {
+  CommandoClient
+} = require('discord.js-commando');
 const path = require('path');
-const {DISCORD_TOKEN} = require('./config.json');
+const {
+  DISCORD_TOKEN
+} = require('./config.json');
 
 const client = new CommandoClient({
-  commandPrefix: '.', 
-  owner: '387156607059886083', 
+  commandPrefix: '.',
+  owner: '387156607059886083',
   invite: ''
 });
 
 client.registry
-     .registerDefaultTypes()
-     .registerGroups([
-       ['first', 'Fun commands.'], 
-       ['second','Admin/Mods only command.'], 
-       ['third', 'Meme commands.']
-       ])
-     .registerDefaultGroups()
-     .registerDefaultCommands({
-       unknownCommand: false
-     })
-     .registerCommandsIn(path.join(__dirname, 'commands'));
-     
-client.once('ready', ()=>{
-  console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
-  client.user.setActivity('with DiscordBot Manager.')
+.registerDefaultTypes()
+.registerGroups([
+  ['first', 'Fun commands.'],
+  ['second', 'Admin/Mods only command.']
+])
+.registerDefaultGroups()
+.registerDefaultCommands({
+  unknownCommand: false
 })
+.registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.once('ready', ()=> {
+  console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+  client.user.setActivity('with DiscordBot Manager.');
+});
+
+client.on('guildMemberAdd', (member)=> {
+  const memberLogChannel = member.guild.channels.cache.find(channel => channel.name === 'member-logs');
+
+  if (!memberLogChannel) return;
+  
+  memberLogChannel.send(`Welcome to the server <@${member.user.id}>!`);
+  const normalRole = member.guild.roles.cache.find(role => role.name === 'Member');
+  member.roles.add(normalRole);
+});
+
+client.on('guildMemberRemove', (member)=> {
+  const serverLog = member.guild.channels.cache.find(channel => channel.name === 'server-logs');
+
+  if(!serverLog) return;
+  serverLog.send(`${member.user.tag} left the server.`);
+  return;
+});
 
 client.on('error', console.error);
 
