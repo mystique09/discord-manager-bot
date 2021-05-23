837f4cd27d6e045c1e8fb00387adee99e46b0b90
mystique09 <knightkrypto7@gmail.com>
2021-05-24 01:06:11 +0800

    added ban command

--- commands/second/ban.js
+++ commands/second/ban.js
@@ -12,14 +12,30 @@ class BanCommand extends Command {
           key: 'member', 
           prompt: 'Who would you like to ban?', 
           type: 'member',
+        }, {
+          key: 'reason', 
+          prompt: 'Provide a reason!',
+          type: 'string'
         }],
-      ownerOnly: true, 
+      clientPermissions: ['ADMINISTRATOR'], 
+      userPermissions: ['BAN_MEMBERS']
     });
   }
   
-  run(message, {member}){
-    message.say('You got banned!');
-    // BAN COMMAND HERE
+  async run(message, {member, reason}){
+    const serverLog = member.guild.channels.cache.find(channel => channel.name === 'server-logs');
+    const member_tag = member.user.tag;
+  
+  if (!serverLog) return;
+    try {
+      const banned = await member.ban({days: 0.000005, reason});
+      if(banned){
+        serverLog.send(`${member_tag} is banned from the server.`);
+        return;
+      }
+    }catch(e){
+      message.say(`You don't have the permission.`);
+    }
   }
 }
 
